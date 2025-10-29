6f02980981ca6e79547667e6d07d7816e430627a
Matheus Schneider <matheusschneidermartins@gmail.com>
2025-10-28 21:53:30 -0300

    aula 7

---
+++ src/app/_components/base-ui/base-ui.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ɵEmptyOutletComponent } from "@angular/router";
+
+@Component({
+  selector: 'app-base-ui',
+  imports: [],
+  templateUrl: './base-ui.html',
+  styleUrl: './base-ui.css',
+})
+export class BaseUi {
+
+}
