83003d96edfda331e0f93e85cbb7e6390ab29cb2
Matheus Schneider <matheusschneidermartins@gmail.com>
2025-11-02 02:03:44 -0300

    Warnings dinamicos

--- src/app/_pages/certificado-form/certificado-form.ts
+++ src/app/_pages/certificado-form/certificado-form.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButton, PrimaryButton],
+  imports: [SecondaryButton, PrimaryButton, FormsModule, CommonModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css',
 })
 export class CertificadoForm {
+  nome: string = "bundao";
+  atividade: string = "";
+  atividades: string[] = ["angular","react","vue","svelte"];
 
+  campoInvalido(control: NgModel){
+    return control.invalid && control.touched;
+  }
 }
